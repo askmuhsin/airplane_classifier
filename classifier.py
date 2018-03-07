dc667e3911528177004a2e8b9d094438c2a9b287
askmuhsin <askmuhsin@gmail.com>
2018-03-07 15:22:11 +0800

    code refactor

--- classifier.py
+++ classifier.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 
-# --------------------------------------------
+## --------------------------------------------
 
 negative_dir = glob.glob('dataset/negative/*.png')
 positive_dir = glob.glob('dataset/positive/*.png')
@@ -27,24 +27,24 @@ for image in positive_dir:
 
 print("# of not_plane:\t", len(not_plane))
 print("# of plane:\t", len(plane))
-# --------------------------------------------
+## --------------------------------------------
 
 X = not_plane + plane
 y = label
 
 assert len(X)==len(y), "mismatch in data and label size"
 
-# Exploratory data
+## Exploratory data
 for _ in range(5):
     n = random.randint(0, len(X)-1)
     img_ = plt.imread(X[n])
-    label_ = "plane" if y[n] else "not plane"
+    label_ = "Exploratory data\n" + ("plane" if y[n] else "not plane")
     plt.imshow(img_)
     plt.title(label_)
     plt.show()
---------------------------------------------
+## --------------------------------------------
 
-# convert to grayscale and flatten data
+## convert to grayscale and flatten data
 def gryAndFlatten(images):
     flatten_data =[]
     for x in images:
@@ -56,30 +56,34 @@ def gryAndFlatten(images):
 X = np.array(gryAndFlatten(X))
 
 assert X.shape[0]==len(X)
-# --------------------------------------------
+## --------------------------------------------
 
-# train test split
+## train test split
 rand_state = np.random.randint(0, 100)
 X_train, X_test, y_train, y_test = train_test_split(
                                 X, y, test_size=0.3, random_state=rand_state)
-# --------------------------------------------
+## --------------------------------------------
 
-## implement support vector classifier
-t = time.time()
-svc = LinearSVC()
-svc.fit(X_train, y_train)
-print("time take to train SVC:\t", time.time()-t)
+# ## implement support vector classifier
+# t = time.time()
+# svc = LinearSVC()
+# svc.fit(X_train, y_train)
+# print("time take to train SVC:\t", time.time()-t)
+#
+# ## save model to disk
+# filename = 'svc_model1.p'
+# pickle.dump(svc, open(filename, 'wb'))
 
-## save model to disk
-filename = 'svc_model1.p'
-pickle.dump(svc, open(filename, 'wb'))
+## load model from disk (if already saved)
+with open('svc_model1.p', 'rb') as f:
+    svc = pickle.load(f)
 
 ## test Accuracy on test set
 print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
 
 ## predict performance on random test set
-n_predict = random.randint(0,len(X_test)-11)
-print('My SVC predicts: ', svc.predict(X_test[n_predict:n_predict-10]))
-print('For these',n_predict, 'labels: ', y_test[n_predict:n_predict-10])
-t2 = time.time()
-print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+n_predict = 10
+t = time.time()
+print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+print('For these',n_predict, 'labels: ', *y_test[0:n_predict])
+print(round(time.time()-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
